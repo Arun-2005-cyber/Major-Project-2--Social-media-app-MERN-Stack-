45929d8901f990d2335226f0f520824990b72121
Arunprakash S <arunprakashsekar2005@gmail.com>
2025-08-27 13:17:22 +0530

    new

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-MONGO_URI = "mongodb+srv://prakashsm940:yFhgxuPPYvTu96ZK@cluster0.v20qocv.mongodb.net/socialmedia?retryWrites=true&w=majority&appName=Cluster0"
+const MONGO_URI = "mongodb+srv://prakashsm940:zj987*h9dKJ_AQj@cluster0.v20qocv.mongodb.net/socialmedia?retryWrites=true&w=majority&appName=Cluster0";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
