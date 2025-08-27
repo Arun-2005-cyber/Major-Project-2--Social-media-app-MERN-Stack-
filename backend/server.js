b74af5b7db21c6c51345f2dafb9319d7603902ab
Arunprakash S <arunprakashsekar2005@gmail.com>
2025-08-27 08:49:46 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // ✅ CORS middleware
 app.use(cors({
-  origin: "https://socia-media.netlify.app/", // your frontend
+  origin: "https://socia-media.netlify.app", // your frontend
   credentials: true
 }));
 
@@ -34,7 +34,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://socia-media.netlify.app/",
+    origin: "https://socia-media.netlify.app",
     credentials: true
   },
 });
