2562328ff10c94403d21fae1ea6ebbcca44d836f
Arunprakash S <arunprakashsekar2005@gmail.com>
2025-08-27 08:38:25 +0530

    new

--- frontend/app/src/api/axios.js
+++ frontend/app/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // will take from .env
+  baseURL: "https://major-project-2-social-media-app-mern.onrender.com", // will take from .env
 });
 
 export default API;
