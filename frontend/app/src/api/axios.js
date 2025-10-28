3349a89c54e8a1af373b3ada9b29482b2c5b4dc6
Arunprakash S <arunprakashsekar2005@gmail.com>
2025-10-28 19:58:47 +0530

    new

--- frontend/app/src/api/axios.js
+++ frontend/app/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://major-project-2-social-media-app-mern.onrender.com", 
-  withCredentials: true, // important if using cookies/session
+  withCredentials: false, // important if using cookies/session
 });
 
 export default API;
