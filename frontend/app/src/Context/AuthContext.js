d0067eb5188f5490a5c2aed32ad180cd3f5e8364
Arunprakash S <arunprakashsekar2005@gmail.com>
2025-10-28 23:21:07 +0530

    new

--- frontend/app/src/Context/AuthContext.js
+++ frontend/app/src/Context/AuthContext.js
@@ -1,11 +1,11 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-
+import {useNavigate} from 'react-router-dom'
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true); // 👈 for initial load
-
+  const navigate = useNavigate();
   // Load user from localStorage when app starts
   useEffect(() => {
     const savedUser = localStorage.getItem("userInfo");
@@ -23,6 +23,7 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     localStorage.removeItem("userInfo");
     setUser(null);
+    navigate('/login');
   };
 
   return (
